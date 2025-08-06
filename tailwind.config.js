d742ed994fdaf2179b831564a2bcbc9cd301336b
criselda <blackcatramen2001@gmail.com>
2025-08-07 00:14:22 +0800

    initialize code base

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        slateGray: "#708090",
+        skyBlue: "#87CEEB",
+        floralWhite: "#FFFAF0",
+        steelBlue: "#4682B4"
+      }
+    }
+  }
+}
