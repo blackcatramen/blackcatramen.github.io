49df62ac0f9d312f5c3a1cc56f95728aee8ec42d
criselda <blackcatramen2001@gmail.com>
2025-08-07 03:04:01 +0800

    no message

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-
 const swiper = new Swiper('.swiper', {
   loop: false,
-  grid: {
-    rows: 2, // two rows of slides
-    fill: 'row', // fills by row, not column
-  },
   spaceBetween: 20,
+  grid: {
+    rows: 2,
+    fill: 'row',
+  },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
   breakpoints: {
     320: {
-      slidesPerView: 1, // 1 per row on very small screens
+      slidesPerView: 1,
     },
     640: {
-      slidesPerView: 2, // 2 per row on small to medium screens
+      slidesPerView: 2,
     },
     1024: {
-      slidesPerView: 3, // 3 per row on large screens
-    }
-  }
+      slidesPerView: 3,
+    },
+  },
 });
 
 
