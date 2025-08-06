c956c83f808587381dc40b08f4f4bf3f913124c2
criselda <blackcatramen2001@gmail.com>
2025-08-07 02:50:50 +0800

    update swiper to multi rows

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 
+// const swiper = new Swiper('.swiper', {
+//     loop: true,
+//     pagination: {
+//         el: '.swiper-pagination',
+//     },
+// });
+
 const swiper = new Swiper('.swiper', {
-    loop: true,
-    pagination: {
-        el: '.swiper-pagination',
-    },
+  loop: false, // turn off loop when using multi-rows (optional)
+  slidesPerView: 2, // how many slides per row
+  grid: {
+    rows: 2, // how many rows
+    fill: 'row', // can also use 'column' if needed
+  },
+  spaceBetween: 20,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
 });
+
